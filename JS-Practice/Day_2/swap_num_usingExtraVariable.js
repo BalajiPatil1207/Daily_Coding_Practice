739afbfcaed23cfda2b97b327369569a36e004e2
Balaji Patil <balajipatil3252@gmail.com>
2026-05-10 11:26:47 +0530

    new que added

---
+++ JS-Practice/Day_2/swap_num_usingExtraVariable.js
@@ -0,0 +1,19 @@
+let num1 = 20;
+let num2 = 30;
+let temp;      // extra variable 
+
+//============ origial numbers ============
+
+console.log("Original Numbers here (Extra Variable): \n", "1st Number :",num1, "\n 2nd Number :",num2);
+
+console.log("\n");
+
+// =========== swap numbers ==============
+
+temp = num1;
+num1 = num2;
+num2 = temp;
+
+console.log("Swap Numbers here : \n", "1st Number :",num1, "\n 2nd Number :",num2);
+
+
