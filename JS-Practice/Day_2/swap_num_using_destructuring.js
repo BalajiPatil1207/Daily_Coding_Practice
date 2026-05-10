da4df7655402d14a52b5702e7fe01b9084b0e7d1
Balaji Patil <balajipatil3252@gmail.com>
2026-05-10 11:30:59 +0530

    swap numbers useing destructure

---
+++ JS-Practice/Day_2/swap_num_using_destructuring.js
@@ -0,0 +1,13 @@
+let num1 = 25;
+let num2 = 90;
+
+//============ origial numbers ============
+
+console.log("Original Numbers here (Destructre): \n", "1st Number :",num1, "\n 2nd Number :",num2);
+
+console.log("\n");
+
+// =========== Destrcture ===============
+
+[num1 , num2] = [num2 , num1];
+console.log("Destructure Numbers here : \n", "1st Number :",num1, "\n 2nd Number :",num2);
