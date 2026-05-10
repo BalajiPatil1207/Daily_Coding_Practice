00f20c9af2dc4d5187bf3f4934a8ab835653ed4a
Balaji Patil <balajipatil3252@gmail.com>
2026-05-10 11:12:49 +0530

    new ques add

--- JS-Practice/Day_2/one_num_one_string.js
+++ JS-Practice/Day_2/one_num_one_string.js
@@ -3,3 +3,12 @@ let str = '5050';
 
 console.log("Data Type is : ", typeof (num + str)); //string
 console.log("Concatenation : ", num + str);  //505050
+
+// ======================== OR ==========================
+
+let num1 = 10;
+let num2 = 20;
+
+console.log("Sum is : ", num1 + num2);  //30
+console.log("Data Type is : ", typeof (num1 + num2)); //number
+
